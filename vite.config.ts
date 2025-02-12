8cc650ffca257d176f091dbc8ab2b7a7e5667f0a
Abootlha <mdzaidtalha6393@gmail.com>
2025-02-12 21:22:13 +0530

    update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
 	server: {
@@ -8,6 +7,6 @@ export default defineConfig({
 	},
 	plugins: [react()],
 	build: {
-		outDir: path.resolve(__dirname, 'build'),
+		outDir: 'dist'
 	}
 });
